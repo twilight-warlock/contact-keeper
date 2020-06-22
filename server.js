544d53b7e44f09333f05b81d0336f006f608794f
twilight-warlock <devansh.ts@somaiya.edu>
2020-06-22 12:03:21 +0530

    Setup to connect with MongoDB Atlas

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
 
 app.get("/", (req, res) => res.send("Hello MERN World"));
 
