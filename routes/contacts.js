e0b5d932120375ff65b7fa25cd453bff8d46b4c2
twilight-warlock <devansh.ts@somaiya.edu>
2020-06-23 13:20:05 +0530

    Create POST route with authentication to add a new contact to the db

--- routes/contacts.js
+++ routes/contacts.js
@@ -23,9 +23,38 @@ router.get("/", auth, async (req, res) => {
 // @route   POST api/contacts
 // @desc    Add new contacts
 // @access  Private
-router.post("/", (req, res) => {
-  res.send("Add contact");
-});
+router.post(
+  "/",
+  [auth, check("name", "Name is required").not().isEmpty()],
+  async (req, res) => {
+    // Checking for Errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      // Creating a new contact by setting values
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+
+      // Saving contact to db
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 // @route   PUT api/contacts/:id
 // @desc    Update contacts
