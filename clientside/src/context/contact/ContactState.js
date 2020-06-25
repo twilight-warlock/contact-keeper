0d8c1c29da39fc414ae230bfa907eb8d764f83ef
twilight-warlock <devansh.ts@somaiya.edu>
2020-06-25 10:56:08 +0530

    Added Contact Form to accept contact information and add created AddContact Reducer

--- clientside/src/context/contact/ContactState.js
+++ clientside/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
+import { v4 } from "uuid";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
 import {
@@ -40,6 +40,13 @@ const ContactState = (props) => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   //   Add Contact
+  const addContact = (contact) => {
+    contact.id = v4();
+    dispatch({
+      type: ADD_CONTACT,
+      payload: contact,
+    });
+  };
 
   //   Delete Contact
 
@@ -57,6 +64,7 @@ const ContactState = (props) => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        addContact,
       }}
     >
       {props.children}
