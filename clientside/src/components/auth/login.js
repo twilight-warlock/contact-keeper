74300f77b84da857b7cd8c53bf58d9786434d1e3
twilight-warlock <devansh.ts@somaiya.edu>
2020-06-26 14:12:06 +0530

    Added User Login functionality

--- clientside/src/components/auth/login.js
+++ clientside/src/components/auth/login.js
@@ -1,19 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
+import AlertContext from "../../context/alert/alertContext";
 
-const Login = () => {
+const Login = (props) => {
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
 
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
+  const { setAlert } = alertContext;
+  const { loginUser, error, clearErrors, isAuthenticated } = authContext;
+
   const { email, password } = user;
+
+  useEffect(() => {
+    // If authenticated, redirect to Home page
+    if (isAuthenticated) {
+      props.history.push("/");
+    }
+
+    if (error === "Invalid Credentials") {
+      setAlert(error, "danger");
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
 
   const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("Login Complete");
+    if (email === "" || password === "") {
+      setAlert("Please fill in all fields", "danger");
+    } else {
+      loginUser({
+        email,
+        password,
+      });
+    }
   };
 
   return (
@@ -33,12 +61,14 @@ const Login = () => {
             name="password"
             value={password}
             onChange={onChange}
+            required
           />
         </div>
         <input
           type="submit"
           value="Login"
           className="btn btn-primary btn-block"
+          required
         />
       </form>
       <Link to="/register">
--- clientside/src/context/auth/AuthState.js
+++ clientside/src/context/auth/AuthState.js
@@ -70,7 +70,28 @@ const AuthState = (props) => {
   };
 
   // Login User
-  const loginUser = () => {};
+  const loginUser = async (formData) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post("/api/auth", formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
 
   // Logout
   const logoutUser = () => {};
