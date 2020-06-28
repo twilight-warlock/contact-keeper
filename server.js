696a387423bb92e01da028369f2f583b8dc63527
twilight-warlock <devansh.ts@somaiya.edu>
2020-06-28 12:41:16 +0530

    Build production

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
@@ -9,11 +10,21 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Hello MERN World"));
+// app.get("/", (req, res) => res.send("Hello MERN World"));
 
 // Defining Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve Static assests in production
+if (process.env.NODE_ENV === "production") {
+  // Set a static folder
+  app.use(express.static("clientside/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "clientside", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
